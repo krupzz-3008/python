bdc11e697423a7bbea7f428a0b2e260465c20990
krupzz-3008 <krupaligangam3008@gmail.com>
2025-08-25 18:35:24 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the First number:"))
+b=int(input("Enter the Second number:"))
+s=a+b
+sub=a-b
+print("addition:",s)
+print("subtraction:",sub)
+m=a*b
+print("multiplication:",m)
+div=a/b
+print("division:",div)
