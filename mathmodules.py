aa903ff2830dd928e0b53db801f5f6c81ba52a85
krupzz-3008 <krupaligangam3008@gmail.com>
2025-09-19 18:14:47 +0530

    Add files via upload

---
+++ mathmodules.py
@@ -0,0 +1,7 @@
+import math
+def modules(n):
+    print("square root of is equal to",math.sqrt(n))
+    print("logarithm of  is equal to",math.log(n))
+    print("sine of  is equal to",math.sin(n))
+print(modules(int(input("enter a number: "))))
+
